e0fee5a3b84a8bb4cd5fb7c3c2a6119ce9a95862
Javier Cabrera <cabrera.javier@gmail.com>
2021-03-26 15:53:42 +0100

    added prettier and eslint with husky

--- App.svelte
+++ App.svelte
@@ -4,9 +4,9 @@
 </script>
 
 <div class="text-align-center antialiased text-gray-900 bg-red-900">
-  <header>
-    <a href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
-      Learn Svelte
-    </a>
-  </header>
+              <header>
+      <a href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
+        Learn Svelte
+      </a>
+              </header>
 </div>
