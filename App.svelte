359fc4fb3329b800d9fbfc9c21b1d727e287fd8d
Javier Cabrera <cabrera.javier@gmail.com>
2021-03-26 15:37:26 +0100

    added tailwindcss

--- App.svelte
+++ App.svelte
@@ -1,12 +1,11 @@
 <script>
   /* component logic will go here */
+  import './Tailwind.svelte'
 </script>
-<style>
-  /* css will go here */
-</style>
-<div class="App">
-  <header class="App-header">
-    <a class="App-link" href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
+
+<div class="text-align-center antialiased text-gray-900 bg-red-900">
+  <header>
+    <a href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
       Learn Svelte
     </a>
   </header>
