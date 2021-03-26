a119d130a1ea509b7d339357c64b9dcc75cee98a
Javier Cabrera <cabrera.javier@gmail.com>
2021-03-26 18:44:23 +0100

    Updates

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+
+/* Add JavaScript code here! */
+import App from './App.svelte.js'
+
+let app = new App({
+  target: document.body,
+})
+
+export default app
---
+++ stores.js
@@ -0,0 +1,5 @@
+import { writable } from './_snowpack/pkg/svelte/store.js'
+
+export const project = writable(0)
+
+export const start = writable(0)
